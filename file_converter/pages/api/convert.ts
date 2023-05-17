f1299b986c22352c14767c637951965468cb95b5
Victor St-Louis <victor.st-louis@hotmail.ca>
2023-05-17 01:09:24 -0400

    Basic UI

--- file_converter/pages/api/convert.ts
+++ file_converter/pages/api/convert.ts
@@ -5,9 +5,7 @@ type Data = {
   name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  console.log(req.body)
   res.status(200).json({ name: 'John Doe' })
 }
