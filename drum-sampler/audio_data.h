998e6baea2663fef5cabecce50303f9b6fcd2ae8
Victor St-Louis <victor.st-louis@hotmail.ca>
2024-01-11 14:00:52 -0500

    renames and refactor

---
+++ drum-sampler/audio_data.h
@@ -0,0 +1,9 @@
+#include <cstdint>
+#ifndef _AUDIO_DATA_H
+#define _AUDIO_DATA_H
+
+extern const size_t AUDIO_DATA_SIZES[];
+extern const size_t AUDIO_DATA_OFFSETS[];
+extern const uint8_t AUDIO_DATA[];
+
+#endif
